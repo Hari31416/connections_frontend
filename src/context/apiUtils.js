a7da0eb4f05574a2c1ea408f0296880857281997
Harikesh Kushwaha <harikeshkumar0926@gmail.com>
2025-05-24 21:07:55 +0530

    add env variables

--- src/context/apiUtils.js
+++ src/context/apiUtils.js
@@ -1,5 +1,5 @@
 // API utility functions
-const API_BASE = "http://localhost:4000/api";
+const API_BASE = process.env.REACT_APP_API_BASE || "http://localhost:4000/api";
 
 export const fetchWithAuth = async (endpoint, options = {}, token) => {
   const url = `${API_BASE}${endpoint}`;
